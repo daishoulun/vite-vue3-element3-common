cf97259fcd84c389c65c53090528803460e44513
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-15 15:45:13 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-container>
+    <el-header>
+      Header
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <div>
+          <router-link to="/">Home</router-link>
+        </div>
+        <div>
+          <router-link to="/about">About</router-link>
+        </div>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container?
+    </el-container>
+  </el-container>
+</template>
+
+<style>
+  
+</style>
