b210a94cba4faff7ef067caeede46e07dc4c12c6
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-15 15:39:38 +0800

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Element3 from 'element3'
+import 'element3/lib/theme-chalk/index.css'
+
+const app = createApp(App)
+
+app.use(router).use(store).use(Element3).mount('#app')
