2241589ee1bea0ed9d33795b1855a4b1f06adc45
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-15 15:20:00 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { fileURIToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURIToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
