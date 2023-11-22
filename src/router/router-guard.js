4dd554af288d504719d9a9b1bd1b453ccfec1305
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-22 09:58:40 +0800

    Update router-guard.js

--- src/router/router-guard.js
+++ src/router/router-guard.js
@@ -1,9 +1,13 @@
 import router from './index'
 
 router.beforeEach(async (to, fro, next) => {
+  const { fullPath } = to
   const token = localStorage.getItem('token')
+  if (fullPath === '/login') {
+    next()
+  }
   if (!token) {
     next('/login')
   }
-  return true
+  next()
 })
