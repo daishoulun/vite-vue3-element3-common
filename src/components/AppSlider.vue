b71d2f13caded8d74718475fbd4e98d082b904d8
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-22 09:30:29 +0800

    Create AppSlider.vue

---
+++ src/components/AppSlider.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="app-slider">
+    <el-menu
+        :default-active="activeIndex"
+        @select="handleSelect"
+      >
+      <el-menu-item index="Home">Home</el-menu-item>
+      <el-menu-item index="About">About</el-menu-item>
+    </el-menu>
+  </main>
+</template>
+
+<script setup>
+  improt { ref } from 'vue'
+  const activeIndex = ref('Home')
+  const handleSelect = (val => {
+    console.log('selectSlider', val)
+  }
+</script>
+
+<style lang="scss" scoped>
+  .app-slider {
+    :deep(.el-menu--vertical) {
+      border-right: none;
+    }
+  }
+</style>
