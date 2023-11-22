ad3a6e7871528d34176c2d7767788d22bc5bc55a
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-22 10:03:29 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,32 @@
 import { crateRouter, createWebHistory } from 'vue-router'
+import Layout from '../components/Layout.vue'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Login from '../views/Login.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      name: 'Layout',
+      component: Layout,
+      children: [
+        {
+          path: '/',
+          name: 'Home',
+          component: Home
+        },
+        {
+          path: '/about',
+          name: 'About',
+          component: About
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'About',
-      component: About
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
