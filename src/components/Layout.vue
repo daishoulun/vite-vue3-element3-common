882305a3cf20c4464d77dd22829ff076b011f11b
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-22 09:25:49 +0800

    Create Layout.vue

---
+++ src/components/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-container>
+    <el-header class="layout-header">Header</el-header>
+    <el-container class="layout-content">
+      <el-aside class="layout-aside">
+        <AppSlider></AppSlider>
+      </el-aside>
+      <el-main class="layout-main">
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
