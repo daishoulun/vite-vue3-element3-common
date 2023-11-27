defdc23d3290b3c182fa67812168cbadad37f55e
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-27 09:11:44 +0800

    feat: add pack analysis

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURIToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx()
+    vueJsx(),
+    visualizer()
   ],
   resolve: {
     alias: {
