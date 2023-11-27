092f420f86184cadeb42e343c8b2843a27a28e61
daishoulun <33917670+daishoulun@users.noreply.github.com>
2023-11-27 10:02:33 +0800

    feat: Full import changed to auto-import

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from './router'
 import './router/router-guard'
 import store from './store'
 import './assets/main.css'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
